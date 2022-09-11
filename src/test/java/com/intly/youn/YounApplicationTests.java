984c928b9a973971c9aa1d2f568cd50f4302be03
taekwon-dev <taekwon_dev@naver.com>
2022-09-11 14:28:09 +0900

    2022/09/11 init

---
+++ src/test/java/com/intly/youn/YounApplicationTests.java
@@ -0,0 +1,13 @@
+package com.intly.youn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YounApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
